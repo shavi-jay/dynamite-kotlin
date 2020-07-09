be37363bfa591ee6eb2542a14fc423b382c12255
shavi-jay <60151734+shavi-jay@users.noreply.github.com>
2020-07-09 09:27:54 +0100

    Update MyBot.kt

--- dynamite/src/main/java/com/example/dynamite/MyBot.kt
+++ dynamite/src/main/java/com/example/dynamite/MyBot.kt
@@ -8,7 +8,19 @@ class MyBot : Bot {
     override fun makeMove(gamestate: Gamestate): Move {
         // Are you debugging?
         // Put a breakpoint in this method to see when we make a move
-        return Move.R
+
+        if (gamestate.rounds.size % 500 == 0 && gamestate.rounds.size > 0) {
+            when (gamestate.rounds[gamestate.rounds.size-1].p1) {
+                Move.P -> return Move.D
+                Move.R -> return Move.D
+                Move.S -> return Move.D
+                Move.D -> return Move.D
+            }
+        } else {
+            return Move.R
+        }
+
+        return Move.S
     }
 
     init {

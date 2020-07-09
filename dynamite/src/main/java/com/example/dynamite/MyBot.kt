288cf6845a4c011cbc43ded67ca3671bf26b46ef
shavi-jay <60151734+shavi-jay@users.noreply.github.com>
2020-07-09 10:45:01 +0100

    New Bot

--- dynamite/src/main/java/com/example/dynamite/MyBot.kt
+++ dynamite/src/main/java/com/example/dynamite/MyBot.kt
@@ -4,28 +4,32 @@ import com.softwire.dynamite.bot.Bot
 import com.softwire.dynamite.game.Gamestate
 import com.softwire.dynamite.game.Move
 
-class MyBot : Bot {
-    override fun makeMove(gamestate: Gamestate): Move {
-        // Are you debugging?
-        // Put a breakpoint in this method to see when we make a move
 
-        if (gamestate.rounds.size % 500 == 0 && gamestate.rounds.size > 0) {
-            when (gamestate.rounds[gamestate.rounds.size-1].p1) {
-                Move.P -> return Move.D
-                Move.R -> return Move.D
-                Move.S -> return Move.D
-                Move.D -> return Move.D
+class MyBot : Bot {
+    private var dynamiteCount = 0
+    override fun makeMove(gamestate: Gamestate): Move {
+
+        val ranFloat = listOf(1,2,3)
+
+        val ran = ranFloat.shuffled().first()
+
+        val ranFloat2 = listOf(1,2,3,4,5,6,7,8)
+
+        val ran2 = ranFloat2.shuffled().first()
+
+        if (dynamiteCount < 100) {
+            if (ran2 == 1) {
+                dynamiteCount += 1
+                return Move.D
             }
-        } else {
-            return Move.R
         }
 
-        return Move.S
-    }
+        return when (ran) {
+            1 -> Move.R
+            2 -> Move.P
+            3 -> Move.S
+            else -> Move.W
+        }
 
-    init {
-        // Are you debugging?
-        // Put a breakpoint on the line below to see when we start a new match
-        println("Started new match")
     }
 }
